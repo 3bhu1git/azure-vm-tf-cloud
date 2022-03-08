08c85892f65d9ac1d51eb1d09d6e70dd1fad7208
Thribhuvan N <38746666+3bhu1git@users.noreply.github.com>
2022-03-08 21:55:15 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "subscription_id" {
+    type =  string
+    default = "b15b429c-cb44-44e0-827f-68d1aebda0b0"
+}
+
+variable "tenant_id" {
+    type =  string
+    default = "f77d0bdc-0d72-4b3d-8d8c-d87448c17ef8"
+}
